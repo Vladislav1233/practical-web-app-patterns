86e72742dbdcd5093e13e8c0f9bcd8a72b19e340
v.dovzhenko <v.dovzhenko@samolet.ru>
2024-09-02 20:40:52 +0300

    Observer Pattern with Mixins

---
+++ todo-list/webapp/mixins.js
@@ -0,0 +1,17 @@
+// Здесь я  применяю два паттерна.
+// Миксин, который будет реализовывать Observer паттерн.
+// В моем слуае мне нужен обзервер, который будет отслеживать изменения в списке задач.
+// По сути я создам поведение (наблюдателя), которое смогу использовать в любом другом месте.
+
+const observerMixin = {
+  observers: new Set(),
+  addObserver(observer) {
+    this.observers.add(observer);
+  },
+  removeObserver(observer) {
+    this.observers.delete(observer);
+  },
+  notify() {
+    this.observers.forEach((observer) => observer.update());
+  },
+};
